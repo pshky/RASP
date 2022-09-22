cae00d652c3f108a548c2d3c6d6f5e9a6ed0c3de
Prasiddha <shakyaprasiddha8@gmail.com>
2022-09-22 18:12:35 +0545

    2nd year project

---
+++ contact.php
@@ -0,0 +1,56 @@
+<?php include "header.php"; ?>	
+	<!-- Start All Pages -->
+		
+		<img src="images/002.jpg" width="100%">
+	<!-- End All Pages -->
+	
+	<!-- Start Contact -->
+	
+	<div class="contact-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Contact</h2>
+						<p>Contact Information are Listed here</p>
+                                                  <p>Thank you for Visiting Our Site!!.....</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<form id="contactForm">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="form-group">
+									<h1 style="font-family: cambria; color: red; text-align: center;">RASP Delivery</h1>
+									<div class="help-block with-errors"></div>
+								</div>                                 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Email Us :</b> RASP_Delivery@gmail.com</span></center>
+									<div class="help-block with-errors"></div>
+								</div> 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group">
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Mobile  :</b> +977 986 21 70967</span></center>
+									<div class="help-block with-errors"></div>
+								</div> 
+							</div>
+							<div class="col-md-12">
+								<div class="form-group"> 
+									<center><span style="color: tomato; font-size: 1.3em; text-align: center; "><b>Location  :</b> jawalakhel, lalitpur.</span></center>
+									<div class="help-block with-errors"></div>
+								</div>
+								
+							</div>
+						</div>            
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Contact -->
+<?php include "footer.php"; ?>
